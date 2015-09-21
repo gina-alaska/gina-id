c8d11d02184bae616bf91cd958c05fc994c037d1
Will Fisher <will@gina.alaska.edu>
2015-09-21 09:17:23 -0800

    setting up phased restarts

--- config/puma.rb
+++ config/puma.rb
@@ -2,7 +2,8 @@ workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 threads_count = Integer(ENV['MAX_THREADS'] || 5)
 threads threads_count, threads_count
 
-preload_app!
+# preload_app!
+prune_bundler
 
 rackup      DefaultRackup
 port        ENV['PORT']     || 9292
@@ -12,5 +13,5 @@ pidfile     ENV['PUMA_PIDFILE'] || '/var/run/puma.pid'
 on_worker_boot do
   # Worker specific setup for Rails 4.1+
   # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
-  ActiveRecord::Base.establish_connection
+  # ActiveRecord::Base.establish_connection
 end
